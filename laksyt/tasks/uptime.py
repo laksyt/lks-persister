700cf4bebc47bb2e4eb511cc855024c9aac6993d
Erik Sargazakov <deftsly@outlook.com>
2021-03-30 10:02:39 +0300

    Polish uptime task

--- laksyt/tasks/uptime.py
+++ laksyt/tasks/uptime.py
@@ -7,15 +7,19 @@ logger = logging.getLogger(__name__)
 
 class UptimeReporter:
 
-    def __init__(self):
+    def __init__(self, period: int = 60):
+        self.period = period
         self.seconds_up = 0.0
         self.uptime = timedelta(seconds=self.seconds_up)
         self.counter = 0
 
     async def report_continuously(self):
         while True:
-            self.seconds_up = asyncio.get_event_loop().time()
-            self.uptime = timedelta(seconds=self.seconds_up)
-            self.counter += 1
-            logger.info(f"Uptime report #{self.counter}: up for {self.uptime}")
-            await asyncio.sleep(60)
+            await self.report_once()
+            await asyncio.sleep(self.period)
+
+    async def report_once(self):
+        self.seconds_up = asyncio.get_event_loop().time()
+        self.uptime = timedelta(seconds=self.seconds_up)
+        self.counter += 1
+        logger.info(f"Uptime report #{self.counter}: up for {self.uptime}")
