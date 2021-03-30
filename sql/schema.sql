f9b4ec5446d242c62e9cae6886e3fea2fc9ae1b5
Erik Sargazakov <deftsly@outlook.com>
2021-03-30 12:27:12 +0300

    Reformat code

--- sql/schema.sql
+++ sql/schema.sql
@@ -5,10 +5,11 @@
 DROP TABLE IF EXISTS target, report CASCADE;
 DROP VIEW IF EXISTS full_report CASCADE;
 
-CREATE TABLE IF NOT EXISTS target (
+CREATE TABLE IF NOT EXISTS target
+(
     target_id serial,
-    url text NOT NULL,
-    needle text DEFAULT NULL,
+    url       text NOT NULL,
+    needle    text DEFAULT NULL,
     PRIMARY KEY (target_id),
     CONSTRAINT uq_url_needle UNIQUE (url, needle)
 );
@@ -16,20 +17,21 @@ CREATE TABLE IF NOT EXISTS target (
 CREATE UNIQUE INDEX url_nullneedle_idx
     ON target (url) WHERE needle IS NULL;
 
-CREATE TABLE IF NOT EXISTS report (
-    report_id bigserial,
-    target_id integer,
-    is_available boolean NOT NULL,
-    status varchar(16) NOT NULL,
-    status_code smallint DEFAULT NULL,
+CREATE TABLE IF NOT EXISTS report
+(
+    report_id     bigserial,
+    target_id     integer,
+    is_available  boolean                  NOT NULL,
+    status        varchar(16)              NOT NULL,
+    status_code   smallint         DEFAULT NULL,
     response_time double precision DEFAULT NULL,
-    needle_found boolean DEFAULT NULL,
-    checked_at timestamp with time zone NOT NULL,
+    needle_found  boolean          DEFAULT NULL,
+    checked_at    timestamp with time zone NOT NULL,
     PRIMARY KEY (report_id),
     CONSTRAINT fk_target
-      FOREIGN KEY (target_id)
-          REFERENCES target (target_id)
-          ON DELETE CASCADE
+        FOREIGN KEY (target_id)
+            REFERENCES target (target_id)
+            ON DELETE CASCADE
 );
 
 CREATE OR REPLACE VIEW full_report AS
@@ -41,4 +43,5 @@ SELECT t.url
      , t.needle
      , r.needle_found
      , r.checked_at
-FROM target t JOIN report r USING (target_id);
+FROM target t
+         JOIN report r USING (target_id);
