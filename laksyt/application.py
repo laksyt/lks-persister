3768ee471e3cced24416a1a5b288c749e1adb4b7
Erik Sargazakov <deftsly@outlook.com>
2021-04-02 12:42:50 +0300

    Improve exception and shutdown handling

--- laksyt/application.py
+++ laksyt/application.py
@@ -33,13 +33,17 @@ class Application:
 
     def launch(self):
         """Kicks off the main workload"""
-        self.loop.create_task(self._workload())
+        workload = self.loop.create_task(self._workload())
         try:
-            self.loop.run_forever()
+            self.loop.run_until_complete(workload)
         except asyncio.CancelledError:
-            self.logger.warning("Cancelling event loop")
+            self.logger.exception("Workload was unexpectedly cancelled")
         except KeyboardInterrupt:
-            self.logger.warning("Interrupting event loop")
+            self.logger.warning("Interrupting workload")
+        except BaseException:
+            self.logger.exception("Shutting down workload due to exception")
+        finally:
+            self._clean_up()
 
     async def _workload(self):
         """Groups long-running asynchronous tasks into a single workload"""
@@ -47,3 +51,31 @@ class Application:
             self.uptime_reporter.report_continuously(),
             self.report_persister.poll_continuously()
         )
+
+    def _clean_up(self):
+        try:
+            self._cancel_tasks()
+            self.loop.run_until_complete(self.loop.shutdown_asyncgens())
+            self.loop.run_until_complete(self.loop.shutdown_default_executor())
+        finally:
+            self.logger.info("Closing event loop")
+            self.loop.close()
+
+    def _cancel_tasks(self):
+        tasks = asyncio.all_tasks(self.loop)
+        if not tasks:
+            self.logger.info("All tasks completed")
+            return
+        self.logger.info("Cancelling remaining tasks")
+        for task in tasks:
+            self.logger.info(f"Cancelling {task.get_coro()}")
+            task.cancel()
+        try:
+            self.loop.run_until_complete(
+                asyncio.gather(*tasks, return_exceptions=True)
+            )
+        except KeyboardInterrupt:
+            pass
+        for task in tasks:
+            if task.cancelled() or task.exception() is not None:
+                continue
